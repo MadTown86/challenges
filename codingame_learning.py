048862a1f453985908c36f58e6367fd9e2ebbeea
MadTown86 <grover.donlon@gmail.com>
2023-02-24 22:22:14 -0600

    Nothing of substance commited.

--- codingame_learning.py
+++ codingame_learning.py
@@ -48,3 +48,59 @@ for x in message:
             break
         elif x == s.pop():
             continue
+
+n = int(input())
+for i in range(n):
+    p0, p1 = [int(j) for j in input().split()]
+    print(int(bin(p0)[2:])+int(bin(p1)[2:]))
+
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+def fact(n):
+    if n == 1:
+        return 1
+    return n * fact(n - 1)
+n = int(input())
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+print(fact(n))
+
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+start = int(input())
+
+arr = [1,1]
+for i in range(start-2):
+    arr += [arr[-1] + arr[-2]]
+print(*arr[::-1])
+
+
+start = int(input())
+nums = []
+def fib(prev_num, num, count):
+    if count > 0:
+        fib(num, num+prev_num, count-1)
+        nums.append(num)
+fib(1,1,start-1)
+nums.append(1)
+print(" ".join(list(map(str,nums))))
+
+import math
+
+n = int(input())
+y = int(input())
+print(math.ceil(n * 1.2 ** y * 4))
+
+n,*z=map(int,open(0))
+m=round(sum(z)/len(z))
+d=5*sorted(abs(x-m)for x in z)[n//2]
+print("Keeping values between",m-d,"and",m+d)
--- springer_challenge_besen.py
+++ springer_challenge_besen.py
@@ -1,7 +1,8 @@
-from random import choice
+import random
 
 import numpy
 from typing import Self
+from typing import Any
 
 
 class Node:
@@ -19,14 +20,14 @@ class Path:
     """
     A flavor of doubly linked list abstraction
     """
-    def __init__(self, head:Node = None, tail: Node = None, current: Node = None)-> None:
+    def __init__(self, current: Node = None) -> None:
         self._head = Node()
         self._tail = Node()
         self._head._nextn = self._tail
         self._tail._prev = self._head
         self._current = self._head
 
-    def _add_element(self, pos: tuple[int, int]) -> Node:
+    def _add_element(self, pos: tuple[int, int], moves: int = None) -> Node:
         """
         As a position is chosen, it will be added to the path.
 
@@ -34,7 +35,9 @@ class Path:
         :param pos:
         :return:
         """
-        new_node = Node(pos = pos, prev=self._current, nextn=self._current.nextn)
+
+        new_node = Node(pos = pos, prev=self._current, nextn=self._current._nextn, _moves=moves)
+        self.current._nextn = new_node
         self._current = new_node
         return self._current
 
@@ -66,7 +69,7 @@ class Path:
 
 
 class CastleQueenSide:
-    def __init__(self, pos: tuple[int, int] = (7, 3)) -> None:
+    def __init__(self, pos: tuple[Any, Any] = None) -> None:
         """
         This constructor builds the chess board using numpy arrays, default filled with 0's.
         It also instantiates the following class attributes
@@ -100,17 +103,43 @@ class CastleQueenSide:
         :param pos:
         """
         self.board = numpy.array(8*[8*[0]])
-        self.board[7][3] = 1
-        self.marked = set()
         self.avail_moves = set()
         self.block_paths = set()
         self.bad_paths = {}
-        self.current_pos = pos
+        self.current_pos = (7, 1)
+        self.marked = set()
+        self.marked.add((7, 1))
         self.win = True if len(self.avail_moves) == 0 and len(self.marked) == 64 else False
+        self.path = Path(Node(pos=pos, _moves=3))
 
-        self.path = Path()
+    def _boardprint(self):
+        """
+        This method prints the current board layout to stdout using black, white and blue emojis.
+        :return:
+        """
+        for x, y in self.avail_moves:
+            self.board[x][y] = 2
+        for x, y in self.marked:
+            self.board[x][y] = 1
+        for row in self.board:
+            q_print = []
+            for place in row:
 
-    def _bad_pathcheck(self, arg: tuple[int, int]) -> bool:
+                if place == 0:
+                    q_print.append("\U0001F535")
+                elif place == 1:
+                    q_print.append("\U000026AB")
+                elif place == 2:
+                    q_print.append("\U000026AA")
+            print(q_print)
+
+        print('\n')
+
+        for x, y in self.avail_moves:
+            self.board[x][y] = 0
+        for x, y in self.marked:
+            self.board[x][y] = 0
+    def _bad_pathcheck(self, arg: tuple[Any, Any]) -> bool:
         """
         Create a check that takes the x, y positional input and checks the current board layout against any
         stored layouts that have proven to be 'points of no return'
@@ -128,7 +157,6 @@ class CastleQueenSide:
 
 
     def _check_moves(self) -> None:
-        del self.avail_moves
         self.avail_moves = set()
         """
         This method checks the available moves for the knight and adds them to the class attribute self.avail_moves
@@ -142,9 +170,9 @@ class CastleQueenSide:
             new_x = x + xm
             new_y = y + ym
             check_pos = (new_x, new_y)
-            if new_x < 0 > new_y or new_x > 7 < new_y:
+            if new_x < 0 or new_x > 7 or new_y < 0 or new_y > 7:
                 continue
-            elif (new_x, new_y) in self.bad_paths.keys:
+            elif (new_x, new_y) in self.bad_paths.keys():
                 if self._bad_pathcheck((new_x, new_y)):
                     continue
                 else:
@@ -155,22 +183,43 @@ class CastleQueenSide:
                 if (new_x, new_y) not in self.marked:
                     self.avail_moves.add(check_pos)
                     continue
+    def _backup_move(self):
+        old_pos = self.current_pos
+        old_x, old_y = self.current_pos[0], self.current_pos[1]
+        self.board[old_x][old_y] = 0
+        self.bad_paths[old_pos] = self.board
+        new_curr = self.path._remove_element(self.path._current._pos)
+        self.marked.remove(self.current_pos)
+        self.current_pos = new_curr._pos
 
     def start(self):
-        if not self.win:
+
+        count = 0
+        while count < 100:
+            self.path._path_print()
+            self._boardprint()
+        # while not self.win:
             self._check_moves()
             if not self.avail_moves:
-                """
-                Create - backtracking algorithm
-                """
+                self._backup_move()
+                while self.path._current._moves == 1:
+                    self._backup_move()
+                count += 1
             else:
-
-                cx, xy = choice(*self.avail_moves)
+                t_choice = random.choice([x for x in self.avail_moves])
+                cx = t_choice[0]
+                xy = t_choice[1]
+                self.path._add_element(pos=(cx, xy), moves=len(self.avail_moves))
+                self.marked.add((cx, xy))
+                self.current_pos = (cx, xy)
+                self.board[cx][xy] = 1
+                count += 1
 
 
 
 if __name__ == "__main__":
-
+    C = CastleQueenSide()
+    C.start()
 
 
 
